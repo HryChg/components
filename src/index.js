2c5e5ffc497fa6b7e5b64d410df781c2ca81bb91
harrychuang <vrjgik5@gmail.com>
2019-06-01 16:25:32 -0700

    finialized the design of CommentDetails

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import CommentDetail from './CommentDetail';
+import faker from "faker";
 
 
 const App = () => {
     return (
         <div className={"ui container comments"}>
-            <CommentDetail author={"Sam"}/>
-            <CommentDetail author={"Alex"}/>
-            <CommentDetail author={"Jane"}/>
+            <CommentDetail author={"Sam"}
+                           timeAgo={"Today at 4:45pm"}
+                           content={"Nice Blog Post!"}
+                           avatar={faker.image.avatar()}
+            />
+            <CommentDetail author={"Alix"}
+                           timeAgo={"Today at 2:00pm"}
+                           content={"Blog More! We love you!"}
+                           avatar={faker.image.avatar()}
+            />
+            <CommentDetail author={"BJ"}
+                           timeAgo={"Yesterday at 5:00pm"}
+                           content={"That is just your opinion ~~ "}
+                           avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
-
-
